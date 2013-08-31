6b513989906e20191124a7aa0733918d853e2420
Brett Sanders <brettrsanders@gmail.com>
2013-08-30 23:55:47 -0500

    add notes for reference on order of steps needed for basic table functionality

--- js/app.js
+++ js/app.js
@@ -1,19 +1,27 @@
 var App = Ember.Application.create();
 
 // Router
+// 1. make resource route for tables
 App.Router.map(function(){
 	this.resource('tables'); // '/#/tables'
 });
 
 // In Ember, the Router is responsible for getting data from
 // the model
+// 2. make router object that loads data
 App.TablesRoute = Ember.Route.extend({
   model: function(){
     return App.Table.find();
   }
 });
 
+// Controllers
+// 4. define tables controller and specify it will manage
+//    a list of objects
+App.TablesController = Ember.ArrayController.extend();
+
 // Models
+// 3. define model and load data from it
 App.Store = DS.Store.extend({
   revision: 11,
   adapter: 'DS.FixtureAdapter'
@@ -29,3 +37,6 @@ App.Table.FIXTURES = [
   {id:5},
   {id:6},
 ];
+
+// 5. (in index.html) create template with special tags
+//    to render the data
