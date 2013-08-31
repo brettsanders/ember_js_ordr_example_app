6a9de95f61ca866005c00543d277bdd5de693ddf
Brett Sanders <brettrsanders@gmail.com>
2013-08-30 23:42:29 -0500

    add Table Model and Fixture Data

--- js/app.js
+++ js/app.js
@@ -18,3 +18,14 @@ App.Store = DS.Store.extend({
   revision: 11,
   adapter: 'DS.FixtureAdapter'
 });
+
+App.Table = DS.Model.extend();
+
+App.Table.FIXTURES = [
+  {id:1},
+  {id:2},
+  {id:3},
+  {id:4},
+  {id:5},
+  {id:6},
+];
